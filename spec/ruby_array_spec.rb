c96f372af3d834d35d8f42b27bb72dfe2054bb16
Rachel Moser <moser.rachel@gmail.com>
2020-06-02 09:19:10 -0500

    update array tests

--- spec/ruby_array_spec.rb
+++ spec/ruby_array_spec.rb
@@ -20,26 +20,28 @@ describe Array do
     expect(subject).to be_empty
   end
   # One-line syntex that does the same as the above test.
-  # Look at the auto-generated doc string when this test is run (in terminal).
+  # Look at the auto-generated doc string when this test file is run (in terminal).
   it { is_expected.to be_empty }
 
-  # WIP
-  context 'can declare a let variable inside a context block' do
+  context 'when a let variablce is declared inside a context block' do
     let(:numbers) { [3, 8, 9] }
 
     it 'should change the length' do
       expect(numbers.length).to eq(3)
-      numbers << 2
-      expect(numbers.length).to eq(4)
+      numbers.pop
+      expect(numbers.length).to eq(2)
     end
   end
+
+  # Look at the order of these 5 tests when this test file is run (in terminal).
+  # Why do you think they are in a different order then these tests are written?
 end
 
 # ASSIGNMENT
 describe Array do
   context 'the third item in the implicit subject' do
     # remove the 'x' before running this test
-    it 'should equal 21' do
+    xit 'should equal 21' do
       # update subject to make this test past
       subject << 13 # REMOVE
       subject << 7 # REMOVE
@@ -48,16 +50,18 @@ describe Array do
     end
   end
 
-  # WIP
   context 'can alter properties of subject' do
     # Make a let variable that will pass the first test.
-    let(:numbers) { [3, 8, 9] } # REMOVE
+    let(:lucky_numbers) { [2, 20, 20] } # REMOVE
 
     # remove the 'x' before running this test
-    it 'should change the length' do
-      # expect(subject.length).to eq(0)
-      # subject << 2
-      expect(numbers.length).to eq(3)
+    xit 'should change the length' do
+      expect(lucky_numbers.length).to eq(3)
+    end
+
+    # remove the 'x' before running this test
+    xit 'should equal 42' do
+      expect(lucky_numbers.sum).to eq(42)
     end
   end
 end
