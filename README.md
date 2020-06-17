ff234b8be9b19cdb66aa41221bb53c7dc92dfd1f
Rachel Moser <moser.rachel@gmail.com>
2020-06-17 15:23:26 -0500

    update and remove answers

--- README.md
+++ README.md
@@ -4,15 +4,11 @@ There is a knowledge gap between reading the rspec documentation and fully under
 ## Running Tests
 Since this repository is full of tests, it is recommended to only run rspec tests on one individual file at at time. For example, to run the first test file from the root of this directory:
 
-<pre><code>
-rpsec spec/01_string_spec.rb
-</code></pre>
+<pre><code>rpsec spec/01_string_spec.rb</code></pre>
 
 Tip: If you have tab completion set-up, you can hit 'tab' after the first few characters of the file name.
 
 ## Set-Up
 This repository already has rspec initialized & has the following line added to the .rspec file to output a more colorful and verbose test.
 
-<pre><code>
---format documentation
-</code></pre>
+<pre><code>--format documentation</code></pre>
--- spec/02_array_spec.rb
+++ spec/02_array_spec.rb
@@ -26,10 +26,10 @@ describe Array do
   # Look at the auto-generated doc string when this test file is run (in terminal).
   it { is_expected.to be_empty }
 
-  context 'when a let variablce is declared inside a context block' do
+  context 'when a let variable is declared inside a context block' do
     let(:numbers) { [3, 8, 9] }
 
-    it 'should change the length' do
+    it 'should change the length from 3 to 2' do
       expect(numbers.length).to eq(3)
       numbers.pop
       expect(numbers.length).to eq(2)
@@ -39,16 +39,16 @@ describe Array do
   # Look at the order of these 5 tests when this test file is run (in terminal).
   # Why do you think they are in a different order then these tests are written?
 
-  # Clarification: one-line tests are only recommended when the matcher aligns exactly with the doc string.
+  # Please note: one-line tests are only recommended when the matcher aligns exactly with the doc string.
   # However, many ruby-ists prefer explicitly writing out the test & not using one-line syntex.
 end
 
 # ASSIGNMENT
 describe Array do
-  context 'the third item in the implicit subject' do
+  context 'when using an implicit subject, the third element' do
     # remove the 'x' before running this test
     xit 'should equal 21' do
-      # update subject to make this test past
+      # update the implicit subject to make this test past
       subject << 13 # REMOVE
       subject << 7 # REMOVE
       subject << 21 # REMOVE
@@ -56,17 +56,16 @@ describe Array do
     end
   end
 
-  context 'can alter properties of subject' do
-    # Make a let variable that will pass the first test.
-    let(:lucky_numbers) { [2, 20, 20] } # REMOVE
+  context 'when using one let varirable on two tests' do
+    # Make a let variable that will pass boths tests.
 
     # remove the 'x' before running this test
-    xit 'should change the length' do
+    xit 'should have length of 3' do
       expect(lucky_numbers.length).to eq(3)
     end
 
     # remove the 'x' before running this test
-    xit 'should equal 42' do
+    xit 'should have sum of 42' do
       expect(lucky_numbers.sum).to eq(42)
     end
   end
