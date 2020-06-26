6e785f8046cd791d5ef9ae2d797068b50f422acd
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-06-26 13:04:27 -0700

    Changed hash keys into symbols

--- spec/04_hash_spec.rb
+++ spec/04_hash_spec.rb
@@ -4,13 +4,13 @@
 describe Hash do
   # It is recommended to implicitly define the subject with an alias
   # https://relishapp.com/rspec/rspec-core/docs/subject/explicit-subject
-  subject(:favorites) { { 'color' => 'blue', 'food' => 'fajitas' } }
+  subject(:favorites) { { color: 'blue', food: 'fajitas' } }
 
   context 'when you implicitly define the subject' do
     it 'should be correct with subject or variable' do
-      expect(subject['food']).to eq('fajitas')
+      expect(subject[:food]).to eq('fajitas')
       # Using the explicit name is semantically easier to read, therefore preferred.
-      expect(favorites['food']).to eq('fajitas')
+      expect(favorites[:food]).to eq('fajitas')
     end
   end
 
@@ -18,26 +18,26 @@ describe Hash do
   # https://relishapp.com/rspec/rspec-expectations/docs/built-in-matchers/include-matcher
   context 'when changing favorite color to forest green' do
     it 'should include green' do
-      favorites['color'] = 'forest green'
-      expect(favorites['color']).to include('green')
+      favorites[:color] = 'forest green'
+      expect(favorites[:color]).to include('green')
     end
   end
 
   context 'when testing for true or false values' do
     let(:car_features) do
       {
-        'remote_start?' => true,
-        'parking_camera?' => true,
-        'assisted_steering?' => false
+        remote_start?: true,
+        parking_camera?: true,
+        assisted_steering?: false
       }
     end
 
     it 'should be true' do
-      expect(car_features['remote_start?']).to be true
+      expect(car_features[:remote_start?]).to be true
     end
 
     it 'should be false' do
-      expect(car_features['assisted_steering?']).to be false
+      expect(car_features[:assisted_steering?]).to be false
     end
   end
 end
@@ -47,11 +47,11 @@ end
 describe Hash do
   subject(:my_car) do
     {
-      'make' => 'Volkswagon',
-      'model' => 'Jetta',
-      'year' => 2017,
-      'parking_camera?' => true,
-      'assisted_steering?' => false
+      make: 'Volkswagon',
+      model: 'Jetta',
+      year: 2017,
+      parking_camera?: true,
+      assisted_steering?: false
     }
   end
 
