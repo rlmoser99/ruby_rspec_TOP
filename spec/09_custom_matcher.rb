f6c76a46e20dc70b00d51a4982c554e776412a17
Rachel Moser <moser.rachel@gmail.com>
2020-06-28 13:45:31 -0500

    update 09 to be custom matcher instead of satisfy

--- spec/09_custom_matcher.rb
+++ spec/09_custom_matcher.rb
@@ -1,24 +1,26 @@
 # frozen_string_literal: true
 
 # rubocop:disable Layout/LineLength
-  
-# You can also create a matcher to reuse
+
+# If you need to test a condition that does not have a built-in matcher, you can create your own.
 # https://relishapp.com/rspec/rspec-expectations/v/3-9/docs/custom-matchers
 
 describe 'defining custom matchers' do
-  context 'reusing a matcher that is in scope' do
+  context 'when reusing a matcher that is in scope' do
     matcher :be_divisible_by_four do
       match { |num| (num % 4).zero? }
     end
-  
+
     it 'is divisible by 4' do
       expect(12).to be_divisible_by_four
     end
 
+    # You can test for the inverse of the matcher
     it 'is not divisible by 4' do
       expect(99).not_to be_divisible_by_four
     end
 
+    # You can even use a custom matcher with 'all'
     it 'works with multiple values' do
       expect([12, 100, 800]).to all(be_divisible_by_four)
     end
@@ -32,37 +34,21 @@ describe 'simple word palindrome test' do
   let(:spaceship) { 'spaceship' }
   let(:rotator) { 'rotator' }
   let(:palindrome) { 'palindrome' }
-  # Let's revisit the palindrome test you used in the previous lesson
-  # Write one custom matcher that you can use to pass all tests
 
-  matcher :be_a_palindrome do
-    match { |string| string.reverse == string }
-  end
+  # Write one custom matcher for a simple word palindrome that will pass all of the following tests
 
-  context 'when racecar is used' do
+  context 'when racecar and rotator is used' do
     # remove the 'x' before running this test
-    it 'should be a palindrome' do
+    xit 'should be a palindrome' do
       expect(racecar).to be_a_palindrome
-    end
-  end
-
-  context 'when spaceship is used' do
-    # remove the 'x' before running this test
-    it 'should not be a palindrome' do
-      expect(spaceship).not_to be_a_palindrome
-    end
-  end
-
-  context 'when rotator is used' do
-    # remove the 'x' before running this test
-    it 'should be a palindrome' do
       expect(rotator).to be_a_palindrome
     end
   end
 
-  context 'when palindrome is used' do
+  context 'when spaceship and palindrome is used' do
     # remove the 'x' before running this test
-    it 'should not be a palindrome' do
+    xit 'should not be a palindrome' do
+      expect(spaceship).not_to be_a_palindrome
       expect(palindrome).not_to be_a_palindrome
     end
   end
---
+++ spec_answers/09_custom_matcher_answer.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# ASSIGNMENT
+
+describe 'simple word palindrome test' do
+  let(:racecar) { 'racecar' }
+  let(:spaceship) { 'spaceship' }
+  let(:rotator) { 'rotator' }
+  let(:palindrome) { 'palindrome' }
+
+  # Write one custom matcher for a simple word palindrome that will pass all of the following tests
+
+  matcher :be_a_palindrome do
+    match { |string| string.reverse == string }
+  end
+
+  context 'when racecar and rotator is used' do
+    # remove the 'x' before running this test
+    it 'should be a palindrome' do
+      expect(racecar).to be_a_palindrome
+      expect(rotator).to be_a_palindrome
+    end
+  end
+
+  context 'when spaceship and palindrome is used' do
+    # remove the 'x' before running this test
+    it 'should not be a palindrome' do
+      expect(spaceship).not_to be_a_palindrome
+      expect(palindrome).not_to be_a_palindrome
+    end
+  end
+end
