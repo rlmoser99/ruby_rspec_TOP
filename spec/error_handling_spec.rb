7a33bfa434803f61c7c67841dc145dc38a71c777
Rachel Moser <moser.rachel@gmail.com>
2020-07-02 19:08:35 -0500

    error handling idea

---
+++ spec/error_handling_spec.rb
@@ -0,0 +1,119 @@
+# frozen_string_literal: true
+
+# NumberPickGame has small, isolated methods that are easy to test
+class NumberPickGame
+  attr_accessor :solution, :count, :guess
+  class InvalidInputError < StandardError; end
+
+  def initialize
+    @solution = rand(0..9)
+  end
+
+  def play_game
+    puts "Let's play a game called 'Guess a random number!'"
+    player_turns
+    final_message
+  end
+
+  def player_turns
+    @count = 0
+    loop do
+      player_guess
+      @count += 1
+      break if game_over?
+    end
+  end
+
+  def player_guess
+    @guess = player_input
+    raise InvalidInputError unless guess.match(/^[0-9]$/)
+  rescue InvalidInputError
+    puts 'Invalid input'
+    retry
+  end
+
+  def game_over?
+    guess == solution.to_s
+  end
+
+  def final_message
+    if @count == 1
+      puts 'LUCKY GUESS!'
+    elsif @count < 4
+      puts "Congratulations! You picked the random number in #{@count} guesses!"
+    else
+      puts "That was hard. It took you #{@count} guesses!"
+    end
+  end
+
+  protected
+
+  def player_input
+    puts 'Choose 1-digit between 0-9'
+    gets.chomp
+  end
+end
+
+# game = NumberPickGame.new
+# game.play_game
+
+describe NumberPickGame do
+  subject(:game) { described_class.new }
+  class InvalidInputError < StandardError; end
+
+  describe '#initialize' do
+    matcher :be_between_zero_and_nine do
+      match { |number| number.between?(0, 9) }
+    end
+
+    it 'is a number between 0 and 9' do
+      solution = game.solution
+      expect(solution).to be_between_zero_and_nine
+    end
+  end
+
+  describe '#game_over?' do
+    context 'when user guess is correct' do
+      it 'is game over' do
+        game.solution = 5
+        game.guess = '5'
+        expect(game).to be_game_over
+      end
+    end
+
+    context 'when user guess is not correct' do
+      it 'is not game over' do
+        game.solution = 5
+        game.guess = '2'
+        expect(game).to_not be_game_over
+      end
+    end
+  end
+
+  describe '#player_guess' do
+    context 'when guess is valid' do
+      it 'raises no errors and guess is correct' do
+        user_input = '3'
+        allow(game).to receive(:player_input).and_return(user_input)
+        expect { game.player_guess }.not_to raise_error
+        guess = game.guess
+        expect(guess).to eq('3')
+        # WARNING: .not_to raise_error(SpecificErrorClass)` risks false positives, since literally any other error would cause the expectation to pass, including those raised by Ruby (e.g. NoMethodError, NameError and ArgumentError), meaning the code you are intending to test may not even get reached. Instead consider using `expect { }.not_to raise_error`
+      end
+    end
+
+    # Can not test for an error that was rescued.
+    context 'when guess is not valid' do
+      it 'rescues the errors and prompts user for valid input' do
+        letter_input = 'd'
+        number_input = '2'
+        allow(game).to receive(:player_input).and_return(letter_input, number_input)
+        expect(game).to receive(:puts)
+        expect(game).to receive(:player_input).twice
+        game.player_guess
+        guess = game.guess
+        expect(guess).to eq('2')
+      end
+    end
+  end
+end
