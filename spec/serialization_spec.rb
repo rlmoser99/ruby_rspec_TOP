51ac7ce3b2010d1528460cd85f6adf83d4fb1e5e
Rachel Moser <moser.rachel@gmail.com>
2020-07-08 13:47:42 -0500

    testing for error handling

--- spec/serialization_spec.rb
+++ spec/serialization_spec.rb
@@ -12,7 +12,6 @@ module Database
   rescue SystemCallError => e
     puts "Error while writing to file #{@filename}."
     puts e
-    # raise SystemCallError
   end
 
   def save_to_yaml
@@ -71,7 +70,7 @@ class CaesarBreaker
   end
 end
 
-# phrase = CaesarBreaker.new('Ebiil, Tloia!')
+# CaesarBreaker.new('Ebiil, Tloia!')
 
 # # https://relishapp.com/rspec/rspec-core/docs/helper-methods/define-helper-methods-in-a-module
 
@@ -82,34 +81,45 @@ end
 describe CaesarBreaker do
   subject(:phrase) { described_class.new('Ebiil, Tloia!') }
 
-  # describe '#initialize' do
-  #   it 'is a CaesarBreaker' do
-  #     expect_any_instance_of(CaesarBreaker).to receive(:create_decode_messages)
-  #     expect(phrase).to be_a(CaesarBreaker)
-  #   end
-  # end
+  describe '#initialize' do
+    it 'is a CaesarBreaker' do
+      expect_any_instance_of(CaesarBreaker).to receive(:create_decode_messages)
+      expect(phrase).to be_a(CaesarBreaker)
+    end
+  end
 
   describe '#save_message' do
-    # it 'responds to save_message' do
-    #   allow(phrase).to receive(:display_file_location)
-    #   expect(phrase).to respond_to(:save_message)
-    # end
-
     it 'saves a file without raising an error' do
       expect_any_instance_of(CaesarBreaker).to receive(:create_decode_messages)
       expect(phrase).to receive(:create_filename)
-      expect(File).to exist('/Users/rmoser/projects/odin_on_rails/ruby_rspec_TOP/spec/cipher/EbiilTloia.yaml')
+      # expect(File).to exist('/Users/rmoser/projects/odin_on_rails/ruby_rspec_TOP/spec/cipher/EbiilTloia.yaml')
       expect(phrase).to receive(:display_file_location)
       expect { phrase.save_message }.not_to raise_error
     end
 
-    # it 'raises an error' do
-    #   expect_any_instance_of(CaesarBreaker).to receive(:create_decode_messages)
-    #   allow(File).to receive(:open).and_raise(Errno::ENOENT)
-    #   # allow(YAML).to receive(:dump).and_raise(Errno::ENOENT)
-    #   # expect { phrase.save_message }.to raise_error
-    #   # expect { phrase.save_message }.to raise(Errno::ENOENT)
-    #   expect { phrase.save_message }.to raise(SystemCallError
-    # end
+    it 'rescues an error' do
+      expect_any_instance_of(CaesarBreaker).to receive(:create_decode_messages)
+      expect(phrase).to receive(:create_filename)
+      allow(File).to receive(:open).and_raise(Errno::ENOENT)
+      expect(phrase).to receive(:puts).twice
+      phrase.save_message
+    end
+
+    it 'rescues an error' do
+      expect_any_instance_of(CaesarBreaker).to receive(:create_decode_messages)
+      expect(phrase).to receive(:create_filename)
+      allow(File).to receive(:open).and_raise(Errno::ENOENT)
+      expect(phrase).to receive(:puts).twice
+      expect { phrase.save_message }.not_to raise_error
+    end
+
+    it 'rescues an error' do
+      expect_any_instance_of(CaesarBreaker).to receive(:create_decode_messages)
+      expect(phrase).to receive(:create_filename)
+      allow(File).to receive(:open).and_raise(Errno::ENOENT)
+      expect(phrase).to receive(:puts).once.with('Error while writing to file .yaml.')
+      # error_message = '#<Errno::ENOENT: No such file or directory>'
+      phrase.save_message
+    end
   end
 end
